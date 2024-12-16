178e0369cf4a724f824bb684625e1018d0fd96e7
anhcraft <huynhduyanh123123@gmail.com>
2024-12-17 00:47:51 +0700

    Auto install required PlaceholderAPI expansions

--- plugin/src/main/java/dev/anhcraft/vouchers/Vouchers.java
+++ plugin/src/main/java/dev/anhcraft/vouchers/Vouchers.java
@@ -21,20 +21,29 @@ import dev.anhcraft.vouchers.storage.player.PlayerDataManager;
 import dev.anhcraft.vouchers.storage.server.ServerDataManager;
 import dev.anhcraft.vouchers.util.ConfigHelper;
 import dev.anhcraft.vouchers.util.PluginLogger;
+import me.clip.placeholderapi.PlaceholderAPIPlugin;
+import me.clip.placeholderapi.expansion.cloud.CloudExpansion;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 
 public final class Vouchers extends JavaPlugin {
     public static final int LATEST_PLAYER_DATA_VERSION = 1;
     public static final int LATEST_SERVER_DATA_VERSION = 1;
     private static Vouchers INSTANCE;
     private static VouchersApiImpl API;
+    private PlaceholderAPIPlugin placeholderApi;
     public VouchersManager vouchersManager;
     public PlayerDataManager playerDataManager;
     public ServerDataManager serverDataManager;
@@ -99,10 +108,19 @@ public final class Vouchers extends JavaPlugin {
 
         ReflectionUtil.setDeclaredStaticField(ApiProvider.class, "api", API);
 
+        placeholderApi = PlaceholderAPIPlugin.getInstance();
+
         new UpdateChecker(this, UpdateCheckSource.SPIGOT, "112837")
                 .setDonationLink("https://paypal.me/lycheene")
                 .checkEveryXHours(12)
                 .checkNow();
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                installExpansions();
+            }
+        }.runTaskLaterAsynchronously(this, 60);
     }
 
     @Override
@@ -128,7 +146,7 @@ public final class Vouchers extends JavaPlugin {
         getServer().getScheduler().runTaskTimerAsynchronously(this, pluginLogger::flush, 60L, 100L);
     }
 
-    public YamlConfiguration requestConfig(String path) {
+    private YamlConfiguration requestConfig(String path) {
         File f = new File(getDataFolder(), path);
         Preconditions.checkArgument(f.getParentFile().exists());
 
@@ -141,5 +159,49 @@ public final class Vouchers extends JavaPlugin {
         }
 
         return YamlConfiguration.loadConfiguration(f);
+    }
+
+    private void installExpansions() {
+        List<CompletableFuture<File>> completableFutures = new ArrayList<>();
+
+        for (String expansion : new String[]{"player"}) {
+            if (placeholderApi.getCloudExpansionManager().getCloudExpansionsInstalled().containsKey(expansion))
+                continue;
+            completableFutures.add(installExpansion(expansion));
+        }
+
+        if (completableFutures.isEmpty()) return;
+
+        getLogger().info("Installing required PlaceholderAPI expansions...");
+
+        CompletableFuture.allOf(completableFutures.toArray(CompletableFuture[]::new))
+          .whenComplete((unused, throwable) -> {
+              if (throwable != null) {
+                  try {
+                      throw throwable;
+                  } catch (Throwable e) {
+                      throw new RuntimeException(e);
+                  }
+              }
+
+              getLogger().info("Expansions are installed. Trying reloading PlaceholderAPI...");
+
+              new BukkitRunnable() {
+                  @Override
+                  public void run() {
+                      placeholderApi.reloadConf(Bukkit.getConsoleSender());
+                  }
+              }.runTask(this);
+          });
+    }
+
+    private CompletableFuture<File> installExpansion(String name) {
+        return placeholderApi.getCloudExpansionManager()
+          .findCloudExpansionByName(name).map((Function<CloudExpansion, CompletableFuture<File>>) expansion -> {
+              if (!expansion.isVerified())
+                  return CompletableFuture.failedFuture(new RuntimeException("Expansion is unverified"));
+              CloudExpansion.Version version = expansion.getVersion(expansion.getLatestVersion());
+              return placeholderApi.getCloudExpansionManager().downloadExpansion(expansion, version);
+          }).orElseGet(() -> CompletableFuture.failedFuture(new RuntimeException("Expansion not exists")));
     }
 }
